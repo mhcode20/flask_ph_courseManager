6348eb2749edebc17965fd16a628805082fb3c30
hridoymh <mhhridoy0c@gmail.com>
2022-11-01 15:08:55 +0600

    v-0.0.1

--- web_app.py
+++ web_app.py
@@ -11,13 +11,7 @@ class vid:
     def done(self):
         self.is_done = True
 
-file = open('data.myd','rb')
-tl = pickle.load(file)
 
-sum = 0
-for i in tl:
-    sum += i.duration
-sum = round(sum)
 class main_page:
     def __init__(self,tl,dur):
         self.totalV = len(tl)
@@ -35,12 +29,6 @@ class main_page:
         self.tDuration -= round(self.ml[id].duration)
         self.hour,self.min = self.hnm(self.tDuration)
         self.ml[id].done()
-
-mp = main_page(tl,sum)
-
-
-app = Flask(__name__)
-
 class vid:
     def __init__(self,name,is_done=False):
         self.name = name
@@ -49,6 +37,13 @@ class vid:
         self.order = int(name[6:8].replace('.',''))
     def done(self):
         self.is_done = True
+
+file = open("final.myd",'rb')
+mp = pickle.load(file)
+
+app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello_world():
@@ -59,4 +54,11 @@ def doneop(id):
     if mp.ml[int(id)-1].is_done != True:
         mp.reset(int(id)-1)
     return redirect("/", code=302)
+
+@app.route("/save_ob")
+def save_ob():
+    new_file = open("final.myd",'wb')
+    pickle.dump(mp,new_file)
+    return redirect("/", code=302)
+
 app.run()
